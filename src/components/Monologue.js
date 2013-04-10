467f0242369f9d6e3a204e862100de1840a7d978
RKdev <jrkeyskbe@gmail.com>
2013-04-09 19:28:45 -0500

    Loaded first set of soundfiles

--- src/components/Monologue.js
+++ src/components/Monologue.js
@@ -11,35 +11,63 @@ Plan10.Component.Monologue = function(gameObject, component) {
     
     //some private state things
     var timeStarted = null;
+    var started = false;
     var finished = false;
-    var mario = [];
-    var luigi = 2;
-    var imgIndex = (-1);
-    var frameDelay = 5000;    
-    var imgChanged = null;
+    var monoArray = [];
+    var monoLast = 12;
+    var monoIndex = (-1);  //init to -1 so that it will increment to start of array upon first loop
+    var imgChanged = 0;
+    var frameDelay = null;    
     var lastTimeDrawn = 0;
-    var imageChanged = false;
     var audio;
     
     var audioPaths = [
-         'assets/plan 10 script01.mp3',
-         'assets/plan 10 script07.mp3',
-         'assets/plan 10 script23.mp3'
+         'assets/plan10intro/script-01.mp3',
+         'assets/plan10intro/script-02.mp3',
+         'assets/plan10intro/script-03.mp3',
+         'assets/plan10intro/script-04.mp3',
+         'assets/plan10intro/script-05.mp3',
+         'assets/plan10intro/script-06.mp3',
+         'assets/plan10intro/script-07.mp3',
+         'assets/plan10intro/script-08.mp3',
+         'assets/plan10intro/script-09.mp3',
+         'assets/plan10intro/script-10.mp3',
+         'assets/plan10intro/script-11.mp3',
+         'assets/plan10intro/script-12.mp3',
+         'assets/plan10intro/script-13.mp3'
     ]; 
 
     var imgPaths = [
             'assets/Mario_Pinball.png',
             'assets/Bombette.jpg',
-            'assets/dice.png'
+            'assets/dice.png',
+            'assets/Mario_Pinball.png',
+            'assets/Bombette.jpg',
+            'assets/dice.png',
+            'assets/Mario_Pinball.png',
+            'assets/Bombette.jpg',
+            'assets/dice.png',
+            'assets/Mario_Pinball.png',
+            'assets/Bombette.jpg',
+            'assets/dice.png',
+            'assets/Mario_Pinball.png'            
     ];    
 
     var timeArray = [
-        16,
+        14,
+        2,
+        6,
         12,
-        13
+        12,
+        4,
+        10,
+        6,
+        12,
+        8,
+        7,
+        12,
+        3
     ];
-
-frameDelay = timeArray[0];
 
 
     //load a bunch of stuff and start
@@ -48,11 +76,8 @@ frameDelay = timeArray[0];
         audio = gameObject.getComponent('audioEmitter');
 
         gameObject.engine.loadAssets(imgPaths, function(loaded_images) {                
-          console.log("hi");
-          console.log(loaded_images);
-          mario = loaded_images;
-          luigi = 2;
-          gameObject.enable(); 
+            monoArray = loaded_images;
+            gameObject.enable(); 
 
         }
 );
@@ -63,16 +88,27 @@ frameDelay = timeArray[0];
     
     //just keep track of state  
     component.$on('engine.update', function(deltaTime) {
+        //if starting, set framedelay to length of first frame
+        if (started === false) {
+            started = true;
+            frameDelay = ((timeArray[0])*1000);
+        }
         
+        console.log("before loop: " + frameDelay);
         //either here, or in the `canvas2d.draw` section, check to figure
         //out when the monologue has finished and set "finished" to true
             if (lastTimeDrawn + frameDelay <= gameObject.engine.time) {
+                console.log("In loop: " + frameDelay);
                 lastTimeDrawn = gameObject.engine.time;
-//                console.log(imgIndex);
-//                console.log("fd" + frameDelay);
-//                console.log("tA" + timeArray[imgIndex]);
-            if (imgIndex < luigi) { imgIndex++; }  else { imgIndex = 0; }
-                audio.playOnce(audioPaths[imgIndex]);
+                
+                //loop through all the arrays.  For now, start over instead of stopping
+                if (monoIndex < monoLast) { monoIndex++; }  else { monoIndex = 0; }
+                
+                //set frameDelay to length of next part of monologue
+                frameDelay = ((timeArray[monoIndex]) * 1000); 
+                
+                //start playing next monologue audio, canvas will draw next image in a second
+                audio.playOnce(audioPaths[monoIndex]);
         }
    
         if (finished) {
@@ -83,21 +119,14 @@ frameDelay = timeArray[0];
             //gameObject.engine.emit('monologue.finished');
         }
 
-        // if ((timeArray[imgIndex] === 1) && imageChanged === false) { audio.playOnce('/assets/Zarathustra.mp3');}
-
 
     });
     
     //actually draw the monologue images/text
     component.$on('canvas2d.draw', function(context) {
         //use the canvas directly to draw the images and the text
-        //context.drawimage()
-  //      audio.playOnce(audioPaths[imgIndex]);
-//audio.playOnce('/assets/Zarathustra.mp3');
-        context.drawImage(mario[imgIndex],500,200,200,200);
-        frameDelay = (timeArray[imgIndex] * 1000);
-       // imageChanged=true;
-  
+        context.drawImage(monoArray[monoIndex],500,200,200,200);
+       // frameDelay = (timeArray[monoIndex] * 1000); 
     });
 };
 Plan10.Component.Monologue.alias = "plan10.monologue";
